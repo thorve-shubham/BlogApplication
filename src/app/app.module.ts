8e2bf5c11c9ae7eb41752382b501d14e6d82ebad
thorve_shubham <thorveshubham@gmail.com>
2020-03-12 16:10:54 +0530

    removed post component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
-import { PostComponent } from './post/post.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     AboutComponent,
-    PostComponent
+   
   ],
   imports: [
     BrowserModule,
