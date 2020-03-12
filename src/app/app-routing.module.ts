b678afab07c472837996df08044741ae243acb24
thorve_shubham <thorveshubham@gmail.com>
2020-03-12 15:41:53 +0530

    added post component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { PostComponent } from './post/post.component';
 
 
 const routes: Routes = [
   {path:'home',component:HomeComponent},
-  {path:'about',component:AboutComponent}
+  {path:'about',component:AboutComponent},
+  {path:'',redirectTo:'home',pathMatch:'full'},
+  {path:'createPost',component:PostComponent}
 ];
 
 @NgModule({
